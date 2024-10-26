f19659bb86b60253525ce880297c125134165b31
Rohan Nethala <97348344+RohanNethala@users.noreply.github.com>
2024-10-25 21:23:03 -0400

    removed sensitive api keys

--- spotify_clone/lib/constants/strings.dart
+++ spotify_clone/lib/constants/strings.dart
@@ -1,4 +1,4 @@
 class CustomStrings {
-  static const String clientId = 'd39a1c485dce4ccf8d0bb200c3cfbd26';
-  static const String clientSecret = 'bf5a0f04e7f24b7693e5fb552616be08';
+  static const String clientId = 'insert key';
+  static const String clientSecret = 'insert secret';
 }
